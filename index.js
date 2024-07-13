d1b1b7540d3b9c842360fa84f443cff8d8cec2fd
Kigo-P <peterkigo33@gmail.com>
2024-07-13 10:59:20 +0300

    last commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  fetch('https://anapioficeandfire.com/api/books')
+  return fetch('https://anapioficeandfire.com/api/books')
   .then(res => res.json())
   .then(books => renderBooks(books))
   
@@ -16,5 +16,5 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks()
 });
